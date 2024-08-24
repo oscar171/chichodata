26362d72e2b54b144637547dd3514fd3fc5c3808
ozkr1 <oscarp171@gmail.com>
2024-08-24 11:51:18 -0400

    activar fecth

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-       /*  $schedule->command('app:fetch-products')->dailyAt('08:00'); */
+        $schedule->command('app:fetch-products')->dailyAt('08:00');
     }
 
     /**
